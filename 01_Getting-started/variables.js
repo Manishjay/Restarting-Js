518dc187e75cd4f9bc4115202e476749bda9cddf
MANISH KUMAR <81527267+Manishjay@users.noreply.github.com>
2023-07-30 16:21:44 +0000

    Playing with variables.

---
+++ 01_Getting-started/variables.js
@@ -0,0 +1,14 @@
+ const uniqueId = 4444;
+ var fName = "Mansih";
+ let sirName = "Singh";
+
+ city = "Dhanbad";
+
+ /*
+Avoid use of var keyword!
+Instead use const and let.
+*/
+
+console.log(uniqueId);
+
+ console.table([uniqueId, fName, sirName]);
