b3d7e19b31d17a50a6265fab9ef494979a0295cb
MANISH KUMAR <81527267+Manishjay@users.noreply.github.com>
2023-07-30 17:21:03 +0000

    DataTypes

---
+++ 01_Getting-started/convertionOperation.js
@@ -0,0 +1,15 @@
+let score = 44
+let score2 = "44"
+console.log(typeof(score))
+console.log(typeof(score2));
+
+//type convertion!
+
+let aNumber = Number(score2)
+console.log(typeof(aNumber))
+console.log(aNumber);
+
+let myname = "manish"
+let converted = Number(myname)
+console.log(typeof(converted));
+console.log(converted);
---
+++ 01_Getting-started/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"  // It treats whole code as newer version than older one.
+
+/*
+Original documentation for javascript is available on
+tc39.es . MDN is provided bt mozilla org.
+*/
+
+let name = "Manish"
+let rollNo = 139
+let isFull = false
+
+/*
+number => 2 ^ 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined => non assigned
+symbol => unique
+
+object
+*/
+
+console.log(typeof isFull);
